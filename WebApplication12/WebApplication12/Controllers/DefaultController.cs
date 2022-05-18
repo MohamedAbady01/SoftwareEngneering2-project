4c6771d85a0140bb18f7b906db82cc0179cc84db
mohamedAabady <moelsayed862001@gmail.com>
2022-05-18 03:02:56 +0200

    view and edit some views

--- WebApplication12/WebApplication12/Controllers/DefaultController.cs
+++ WebApplication12/WebApplication12/Controllers/DefaultController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using WebApplication12.Data;
 namespace WebApplication12.Controllers
 {
+    
     public class DefaultController : Controller
     {
+        private SoftwareProject_DBEntity db = new SoftwareProject_DBEntity();
         // GET: Default
         public ActionResult Index()
         {
-            return View();
+            var ProductRec = db.Products.ToList();
+            return View(ProductRec);
+
         }
 
 
